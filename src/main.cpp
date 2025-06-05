d685f8f8426395ae98e049fe54fa496282dda026
Try Except <sjaramillo5@uc.cl>
2025-06-05 11:52:40 -0400

    Include BoardMaster in main

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,6 @@
 #include "esp_system.h"
 
 #define TINY_GSM_MODEM_SIM7600
-#define UART_PORT_M       UART_NUM_1
-#define BUF_SIZE        512
 #define MAX_PHOTOS      4
 
 #include <HTTPClient.h>
@@ -83,55 +81,12 @@ void list_photos(void) {
     closedir(dir);
 }
 
-void uart_receiver_task(void *arg) {
-    uint8_t comm;
-    uint8_t len_body = 0;
-    uint8_t body[BUF_SIZE];
-    uint8_t crc_rx;
-
-    while (1) {
-        int read = uart_read_bytes(UART_PORT_M, &comm, 1, portMAX_DELAY);
-        if (read != 1) continue;
-
-        uint8_t crc = comm;
-
-        if (uart_read_bytes(UART_PORT_M, &len_body, 1, pdMS_TO_TICKS(20)) == 1) {
-            crc ^= len_body;
-
-            if (len_body > 0) {
-                int body_len = uart_read_bytes(UART_PORT_M, body, len_body, portMAX_DELAY);
-                if (body_len != len_body) {
-                    ESP_LOGE(TAG, "❌ Body incompleto");
-                    continue;
-                }
-
-                for (int i = 0; i < len_body; i++) {
-                    crc ^= body[i];
-                }
-            }
-        } else {
-            len_body = 0;
-        }
-
-        if (uart_read_bytes(UART_PORT_M, &crc_rx, 1, portMAX_DELAY) != 1) {
-            ESP_LOGE(TAG, "❌ No se recibió CRC");
-            continue;
-        }
-
-        if (crc == crc_rx) {
-            ESP_LOGI(TAG, "✅ CRC OK | COMM: 0x%02X | LEN: %d", comm, len_body);
-            if (len_body > 0) {
-                ESP_LOG_BUFFER_HEXDUMP(TAG, body, len_body, ESP_LOG_INFO);
-                save_photo(body, len_body);  // Guardar como imagen individual
-            }
-        } else {
-            ESP_LOGE(TAG, "❌ CRC FAIL: local 0x%02X vs recibido 0x%02X", crc, crc_rx);
-        }
-    }
-}
 
 void setup() {
     master.init();
+    master.begin();
+
+    xTaskNotify(master.talk_task, 0x0, eSetValueWithOverwrite);
 }
 
 void loop () {}
